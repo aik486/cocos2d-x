8df53feba2f9519181c9771f378d206d956bd639
Alexandra Cherdantseva <neluhus.vagus@gmail.com>
2020-10-12 12:32:16 +0300

    fix for emscripten

--- cocos/platform/CCPlatformConfig.h
+++ cocos/platform/CCPlatformConfig.h
@@ -59,7 +59,7 @@ THE SOFTWARE.
 #define CC_TARGET_PLATFORM             CC_PLATFORM_UNKNOWN
 
 // Apple: Mac and iOS
-#if defined(__APPLE__) && !defined(ANDROID) // exclude android for binding generator.
+#if defined(__APPLE__) && !defined(ANDROID) && !defined(EMSCRIPTEN) // exclude android for binding generator.
     #include <TargetConditionals.h>
     #if TARGET_OS_IPHONE // TARGET_OS_IPHONE includes TARGET_OS_IOS TARGET_OS_TV and TARGET_OS_WATCH. see TargetConditionals.h
         #undef  CC_TARGET_PLATFORM
