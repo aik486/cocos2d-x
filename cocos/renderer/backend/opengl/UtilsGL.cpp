032391347ada1aff12ec2853a9e85c23a1f547d3
Aleksandr Kozhevnikov <lxkzh.hh@gmail.com>
2024-05-07 11:44:04 +0200

    remove check for REPEAT texture size

--- cocos/renderer/backend/opengl/UtilsGL.cpp
+++ cocos/renderer/backend/opengl/UtilsGL.cpp
@@ -185,11 +185,6 @@ GLint UtilsGL::toGLMinFilter(SamplerFilter minFilter, bool hasMipmaps, bool isPo
 GLint UtilsGL::toGLAddressMode(SamplerAddressMode addressMode, bool isPow2)
 {
     GLint ret = GL_REPEAT;
-    if (!isPow2 && (addressMode != SamplerAddressMode::CLAMP_TO_EDGE))
-    {
-        CCLOG("Change texture wrap mode to CLAMP_TO_EDGE since non-power-of-two texture occur in %s %s %d", __FILE__, __FUNCTION__, __LINE__);
-        return GL_CLAMP_TO_EDGE;
-    }
 
     switch (addressMode)
     {
